c97000b571c4fbe1ed6d5cce6c43b6ddd6cccff6
kazGuido <kazihise.guy@gmail.com>
2024-04-28 16:33:31 +0800

    supabase added

--- pages/user.vue
+++ pages/user.vue
@@ -5,6 +5,6 @@ const user = useSupabaseUser()
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
     <Account v-if="user" />
-    <Auth />
+    <Auth v-else />
   </div>
 </template>
