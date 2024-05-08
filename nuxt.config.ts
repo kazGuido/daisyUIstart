f64cb4eb1f8915bbbd5d418006b182a575758c73
kazGuido <kazihise.guy@gmail.com>
2024-05-08 12:47:44 +0800

    redoing pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,6 +52,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_URL || 'https://8f7532c3-ae66-446a-9029-19b1eeebab3b-00-30hfb3eka4tfa.kirk.replit.dev',
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     }
   },
   app: {
